49b9426e589147573cd72be7e85250f2ea5ef55b
jidonghao <ji534@outlook.com>
2022-03-03 00:46:25 +0800

    练习 计算当前时间

--- python01/练习/2.计算当时时间.py
+++ python01/练习/2.计算当时时间.py
@@ -2,8 +2,6 @@
     输入一个秒值，算出距离当日午夜的时间
     格式：HH:MM:SS
 """
-
-
 class TimeStruct:
     def __init__(self):
         self.hour = 11
@@ -13,25 +11,28 @@ class TimeStruct:
 
 inTime = TimeStruct()
 inTime.second = int(input("请输入秒："))
-# 计算格式化后的时间
-inTime.hour = inTime.second // 3600  # 地板除得小时
-inTime.second -= inTime.hour * 3600
-inTime.minute = inTime.second // 60  # 地板除得分钟
-inTime.second -= inTime.minute * 60
+if inTime.second == 0:
+    print("现在的时间是：00:00:00")
+else:
+    # 计算格式化后的时间
+    inTime.hour = inTime.second // 3600  # 地板除得小时
+    inTime.second -= inTime.hour * 3600
+    inTime.minute = inTime.second // 60  # 地板除得分钟
+    inTime.second -= inTime.minute * 60
 
-nowTime = TimeStruct()
-if inTime.hour > 0:
-    nowTime.hour -= inTime.hour
-if inTime.minute > 0:
-    nowTime.minute -= inTime.minute
-if inTime.second > 0:
-    nowTime.second -= inTime.second
+    nowTime = TimeStruct()
+    if inTime.hour > 0:
+        nowTime.hour -= inTime.hour
+    if inTime.minute > 0:
+        nowTime.minute -= inTime.minute
+    if inTime.second > 0:
+        nowTime.second -= inTime.second
 
-# 最后整理该时间
-if nowTime.second == 60:
-    nowTime.second = 0
-    nowTime.minute += 1
-if nowTime.minute == 60:
-    nowTime.minute = 0
-    nowTime.hour += 1
-print("现在的时间是：%02d:%02d:%02d" % (nowTime.hour, nowTime.minute, nowTime.second))
+    # 最后整理该时间
+    if nowTime.second == 60:
+        nowTime.second = 0
+        nowTime.minute += 1
+    if nowTime.minute == 60:
+        nowTime.minute = 0
+        nowTime.hour += 1
+    print("现在的时间是：%02d:%02d:%02d" % (nowTime.hour, nowTime.minute, nowTime.second))
