78a5ea7712712a99b86e07f2c2dbbc2cab9a5c72
jidonghao <ji534@outlook.com>
2022-03-16 11:13:30 +0800

    22316

--- python01/学生成绩.py
+++ python01/学生成绩.py
@@ -1,6 +1,6 @@
-#计算学生成绩
+# 计算学生成绩
 math = float(input("请输入数学成绩："))
 chinese = float(input("请输入语文成绩："))
 english = float(input("请输入英语成绩："))
-sum = math + chinese + english
-print("总分是：%3.2f 平均：%2.2f"%(sum,sum/3))
+s = math + chinese + english
+print("总分是：%3.3f 平均：%2.3f" % (s, s / 3))
