fa6b7c061dd7b7e2a8a58298a128f176af8429ea
jidonghao <ji534@outlook.com>
2022-03-23 10:49:24 +0800

    3月23日

--- python02/3月23练习/判断车票.py
+++ python02/3月23练习/判断车票.py
@@ -1,8 +1,9 @@
-# his_ticket = True
-
-
 his_ticket = input("有票吗?：")
-if "没" in his_ticket or "n" in his_ticket or "F" in his_ticket or "f" in his_ticket:
+if "没" in his_ticket \
+        or "n" in his_ticket \
+        or "N" in his_ticket \
+        or "F" in his_ticket \
+        or "f" in his_ticket:
     his_ticket = False
 if his_ticket:
     print("允许安检")
