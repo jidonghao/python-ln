30eed8e68931af9553b2dda2705db79b42f1b849
jidonghao <ji534@outlook.com>
2022-04-13 09:02:47 +0800

    异常捕获

---
+++ python02/4月13练习/1.异常捕获.py
@@ -0,0 +1,13 @@
+try:
+    name = input("姓名:")
+    if name.strip() == "":
+        raise Exception("无效的姓名")
+    gender = input("性别:")
+    if gender != "男" and gender != "女":
+        raise Exception("无效的性别")
+    age = float(input("年龄:"))
+    if age < 18 or age > 30:
+        raise Exception("无效的年龄")
+    print(name,gender,age)
+except Exception as err:
+    print(err)
