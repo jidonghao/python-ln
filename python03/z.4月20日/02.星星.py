d49874163524ac7870f68615d7fd70cbe0199329
jidonghao <ji534@outlook.com>
2022-04-20 09:46:55 +0800

    上传

--- python03/z.4月20日/02.星星.py
+++ python03/z.4月20日/02.星星.py
@@ -1,11 +1,11 @@
-def print_triangle():
-    for i in range(4):
-        for j in range(0, 4 - i):
-            print(" ", end="")
-        for k in range(4 - i, 5):
+def print_triangle(n):
+    for i in range(n + 1):
+        for j in range(0, (n + 1) - i):
+            print(end=" ")
+        for k in range((n + 1) - i, (n + 1)):
             print("*", end=" ")
 
         print("")
 
 
-print_triangle()
+print_triangle(4)
